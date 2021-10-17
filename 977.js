99f15010245353bcc436c909a2e174d76c0685ef
ykpark <read0more@gmail.com>
2021-10-17 21:15:52 +0900

    Solve 977

---
+++ 977.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function (nums) {
+  return nums.map((num) => num * num).sort((a, b) => a - b);
+};
+
+console.log(sortedSquares([-4, -1, 0, 3, 10]));
