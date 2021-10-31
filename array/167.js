b249fa451d6d95c74c54b26b11876df23e76ebdd
ykpark <read0more@gmail.com>
2021-10-31 14:54:29 +0900

    solve 167, 283

---
+++ array/167.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function (numbers, target) {
+  for (let i = 0; i < numbers.length; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      if (numbers[i] + numbers[j] === target) {
+        return [i + 1, j + 1];
+      }
+    }
+  }
+};
+
+a = twoSum([2, 7, 11, 15], 9);
+console.log(a);
