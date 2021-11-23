7f1ba1083368666e63bf68db5e58dd9db95ed2df
ykpark <read0more@gmail.com>
2021-11-23 21:32:44 +0900

    Improve runtime ms 167

--- array/167.js
+++ array/167.js
@@ -4,11 +4,17 @@
  * @return {number[]}
  */
 var twoSum = function (numbers, target) {
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = i + 1; j < numbers.length; j++) {
-      if (numbers[i] + numbers[j] === target) {
-        return [i + 1, j + 1];
-      }
+  let left = 0;
+  let right = numbers.length - 1;
+
+  while (left < right) {
+    const sum = numbers[left] + numbers[right];
+    if (sum === target) {
+      return [++left, ++right];
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
     }
   }
 };
