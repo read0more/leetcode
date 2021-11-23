32839849ed0f8d5028fcd4a0a7a67bafed438d15
ykpark <read0more@gmail.com>
2021-11-23 21:23:32 +0900

    Improve runtime ms 283

--- array/283.js
+++ array/283.js
@@ -3,12 +3,19 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function (nums) {
-  for (let i = nums.length - 1; 0 <= i; i--) {
-    if (nums[i] === 0) {
-      nums.splice(i, 1);
-      nums.push(0);
+  let cnt = 0;
+
+  for (const num of nums) {
+    nums[cnt] = num;
+    if (num !== 0) {
+      cnt++;
     }
+  }
+
+  for (let i = cnt; i < nums.length; i++) {
+    nums[i] = 0;
   }
 };
 
 moveZeroes([0, 0, 1]);
+moveZeroes([0, 1, 0, 3, 12]);
