8da4226e7acf89136513a3b8d88f50aa504f9886
ykpark <read0more@gmail.com>
2022-05-04 17:42:50 +0900

    34

--- binary-search/34.js
+++ binary-search/34.js
@@ -48,3 +48,38 @@ console.log(searchRange([], 0));
 console.log(searchRange([2], 2));
 console.log(searchRange([2, 2], 2));
 console.log(searchRange([1, 3], 1));
+
+
+// searchRange2보다 searchRange가 정말 약간 빠르긴 하다.
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange2 = function (nums, target) {
+    function find(nums, target, low = 0, high = nums.length) {
+        while (low <= high) {
+            const mid = low + high >> 1;
+            const current = nums[mid];
+            if (current < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+
+        return low;
+    }
+
+    const low = find(nums, target);
+    if (nums[low] !== target) return [-1, -1];
+    return [low, find(nums, target + 1, low) - 1];
+}
+
+
+// console.log(searchRange2([5, 7, 7, 8, 8, 10], 8));
+// console.log(searchRange2([5, 7, 7, 8, 8, 10], 6));
+// console.log(searchRange2([], 0));
+// console.log(searchRange2([2], 2));
+// console.log(searchRange2([2, 2], 2));
+console.log(searchRange2([1, 2, 5, 7, 8], 5));
